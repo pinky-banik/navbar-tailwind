2232f8449233efb749332bf62efb97a73845180f
pinky-banik <pinky.rani.banik>
2023-04-02 15:48:30 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/Button";
+import Navbar from "./components/Navbar/Navbar";
+import ShortNav from "./components/Navbar/ShortNav";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section
+
+      className="h-screen 
+      font-[Poppins] "
+    >
+      <ShortNav/>
+      <Navbar />
+      
+    </section>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar/ShortNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShortNav = () => {
+    return (
+        <nav className='p-1 bg-teal-500 justify-between flex'>
+            <p className='px-5'>Adress : bla bla bla</p>
+            <p className='px-5'>phone : 017000000000</p>
+        </nav>
+    );
+};
+
+export default ShortNav;
